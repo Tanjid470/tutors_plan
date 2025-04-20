8c59d287dcceef1f5f946e5981f7b3e83c5008c3
Tanjid470 <tanjidamran11@gmail.com>
2025-04-20 17:59:25 +0600

    feat : Added dashboard view with appbar

---
+++ lib/feature/dashboard/dashboard_view.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard'),),
+    );
+  }
+
+}
