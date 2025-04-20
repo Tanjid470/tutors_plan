fbe440c2df7bf4d4ef2921ec8f80009e90469431
Tanjid470 <tanjidamran11@gmail.com>
2025-04-20 18:14:01 +0600

    feat : Added dashboard view and logout functionality from dashboard view

--- lib/feature/dashboard/dashboard_view.dart
+++ lib/feature/dashboard/dashboard_view.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:tutors_plan/main.dart';
+import 'package:tutors_plan/route/app_pages.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
@@ -14,7 +13,19 @@ class _DashboardViewState extends State<DashboardView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Dashboard'),),
+      appBar: AppBar(
+        centerTitle: true,
+        leading: null,
+        title: Text('Dashboard'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await preferences.clear();
+              Navigator.pushReplacementNamed(context, RouteNames.loginView);
+            }
+          , icon: Icon(Icons.logout,color: Colors.red,),)
+        ],
+      ),
     );
   }
 
