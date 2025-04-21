deb35ee4b6a322167933839305c85eca1378ada5
Tanjid470 <tanjidamran11@gmail.com>
2025-04-21 11:46:20 +0600

    feat : Change testApiVersion and serverUrl value and added loginEndpoint with serverUrl and added network helper to print api logs

--- lib/const/url_const.dart
+++ lib/const/url_const.dart
@@ -1,8 +1,10 @@
 class UrlConst{
   static final String betaApiVersion = '';
   static final String productionApiVersion = '';
-  static final String testApiVersion = '';
+  static final String testApiVersion ='https://api.tutorsplan.com';
 
-  static final String serverUrl = '';
-  static final String login = '';
+  static final String serverUrl = testApiVersion;
+
+
+  static final String loginEndpoint = '$testApiVersion/login';
 }
