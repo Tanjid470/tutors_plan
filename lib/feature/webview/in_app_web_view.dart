01c02ff906d35d15bced388d03727e4560ded56f
Tanjid470 <tanjidamran11@gmail.com>
2025-04-24 12:53:41 +0600

    [CREATE] : Added InAppWebView widget to display web content using webview_flutter package. It handles loading state and resource errors.

---
+++ lib/feature/webview/in_app_web_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class InAppWebView extends StatefulWidget {
+  const InAppWebView({super.key});
+
+  @override
+  State<InAppWebView> createState() => _InAppWebViewState();
+}
+
+class _InAppWebViewState extends State<InAppWebView> {
+  late final WebViewController _controller;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onWebResourceError: (error) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://games.tutorsplan.com'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            WebViewWidget(controller: _controller),
+            if (isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
