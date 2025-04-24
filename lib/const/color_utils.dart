7165b8df576fb5be9064904d746fdb1821c8b55b
Tanjid470 <tanjidamran11@gmail.com>
2025-04-24 14:31:53 +0600

    [MODIFY] : Replace baseColor with orange shade500 from Colors class

--- lib/const/color_utils.dart
+++ lib/const/color_utils.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorUtils{
   ColorUtils._();
-  static Color baseColor = const Color(0xff8346AD);
+  static Color baseColor = Colors.orange.shade500;
   static Color baseColorShadow = const Color(0xffE0C6FD);
   static Color blackColor = const Color(0xff213346);
   static Color greyColor = const Color(0xffD0CDCE);
