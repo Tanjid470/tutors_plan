e8468ae539b8b6f11e9f4489ba572be89c68f368
Tanjid470 <tanjidamran11@gmail.com>
2025-04-24 14:32:29 +0600

    [ADDED] : Added ScreenStates for login and added logic for update screen state with validation.

--- lib/feature/login/controller/login_controller.dart
+++ lib/feature/login/controller/login_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
+import 'package:tutors_plan/const/enums.dart';
 import 'package:tutors_plan/feature/login/data/login_response_body.dart';
 import 'package:tutors_plan/feature/login/data/repository/login_repository.dart';
 import 'package:tutors_plan/feature/login/domain/login_body.dart';
@@ -9,6 +10,9 @@ import 'package:tutors_plan/main.dart';
 import 'package:tutors_plan/route/app_pages.dart';
 
 class LoginController extends GetxController{
+  final Rx<ScreenStates> screenStates = Rx<ScreenStates>(ScreenStates.DEFAULT);
+  final Rx<ScreenStates> loaderState = Rx<ScreenStates>(ScreenStates.DEFAULT);
+
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   RxString emailError = ''.obs;
@@ -17,18 +21,15 @@ class LoginController extends GetxController{
   LoginBody loginBody = LoginBody();
 
   Future<void> login(BuildContext context) async{
-    try {
-      //validationCheck();
-      await insertLoginBody();
-      LoginResponseBody? response = await loginRepository.fetchLoginResponse(loginBody);
-      if (response?.status == 'SUCCESS') {
-        preferences.setInt('initScreen', 1);
-        Navigator.pushReplacementNamed(context, RouteNames.dashboardView);
-      }
-    } catch (e) {
-      SmartDialog.dismiss();
-      SmartDialog.showToast('Login failed: $e');
+    updateViewState(loadingState: ScreenStates.TRANSPARENT_LOADING_START);
+    //validationCheck();
+    await insertLoginBody();
+    LoginResponseBody? response = await loginRepository.fetchLoginResponse(loginBody);
+    if (response?.status == 'SUCCESS') {
+      preferences.setInt('initScreen', 1);
+      Navigator.pushReplacementNamed(context, RouteNames.dashboardView);
     }
+    updateViewState(screenStates: ScreenStates.LOADING_COMPLETE);
   }
 
   insertLoginBody() {
@@ -54,5 +55,14 @@ class LoginController extends GetxController{
     } else {
       passwordError.value = '';
     }
+  }
+
+  void updateViewState({ScreenStates? screenStates, ScreenStates? loadingState}) {
+    if (screenStates != null) {
+      this.screenStates.value = screenStates;
+      loaderState.value = ScreenStates.LOADING_COMPLETE;
+    }
+    if (loadingState != null) loaderState.value = loadingState;
+    update();
   }
 }
