fc052157b60955be4f3d712e5966227f5f164fc4
Tanjid470 <tanjidamran11@gmail.com>
2025-04-26 13:36:34 +0600

    [ADDED] : Add black87 and input border color to ColorUtils

--- lib/const/color_utils.dart
+++ lib/const/color_utils.dart
@@ -13,6 +13,7 @@ class ColorUtils{
   static Color basePurpleColor = Color(0xff8c00db);
 
   static Color grey300 = Colors.grey.shade300;
+  static Color black87 = Colors.black87;
 
   static Color inputBorderColor = Colors.grey.shade300;
   static Color errorBorderColor = Colors.red;
