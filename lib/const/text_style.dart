c61333c8211d2a2327fd2b2d263db26c0669b9fb
Tanjid470 <tanjidamran11@gmail.com>
2025-04-26 13:23:08 +0600

    [CREATE] : create global custom text styles with white and base blue color options.

---
+++ lib/const/text_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tutors_plan/config/font_constants.dart';
+import 'package:tutors_plan/const/color_utils.dart';
+
+TextStyle whiteText(double fontSize){
+  return  TextStyle(
+      fontSize: fontSize,
+      fontWeight: FontWeight.w700,
+      color:Colors.white
+  );
+}
+TextStyle customTextStyle(context,{Color? color,double? fontSize}){
+  return  TextStyle(
+      fontSize: fontSize ?? TextSize.font14(context),
+      fontWeight: FontWeight.w500,
+      color: color ?? ColorUtils.baseBlueColor
+  );
+}
