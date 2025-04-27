305a56628a3ba3588c2aa8950341789baefde21b
Tanjid470 <tanjidamran11@gmail.com>
2025-04-27 16:29:30 +0600

    [ADDED] : Add UserRole class with title, imageUrl, selectedColor, and bgColor properties

---
+++ lib/feature/register/domain/user_role.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class UserRole {
+  final String title;
+  final String imageUrl;
+  final Color selectedColor;
+  final Color bgColor;
+
+  UserRole({
+    required this.title,
+    required this.imageUrl,
+    required this.selectedColor,
+    required this.bgColor,
+  });
+}
