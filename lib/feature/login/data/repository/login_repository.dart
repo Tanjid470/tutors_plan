f0cc60d59c05015814e8d06afe9720b851d0f54f
Tanjid470 <tanjidamran11@gmail.com>
2025-04-27 12:39:54 +0600

    [ADDED] : Implement APIResult and handle status codes in login repository.

--- lib/feature/login/data/repository/login_repository.dart
+++ lib/feature/login/data/repository/login_repository.dart
@@ -4,24 +4,35 @@ import 'package:tutors_plan/const/url_const.dart';
 import 'package:tutors_plan/feature/login/data/login_response_body.dart';
 import 'package:tutors_plan/feature/login/domain/login_body.dart';
 import 'package:tutors_plan/utils/network/api_client.dart';
+import 'package:tutors_plan/utils/network/api_result.dart';
 
 class LoginRepository {
   Dio _dio = Dio();
 
-  Future<(LoginResponseBody?, Map<String, dynamic>?)> fetchLoginResponse(LoginBody loginBody) async {
+  Future<ApiResult<LoginResponseBody>> fetchLoginResponse(LoginBody loginBody) async {
     try {
       _dio = await ApiClient.dioClient(false);
-      var response = await _dio.post(UrlConst.loginEndpoint, data: loginBody.toJson());
+      Response response = await _dio.post(
+        UrlConst.loginEndpoint,
+        data: loginBody.toJson(),
+        options: Options(
+          validateStatus: (status) => status != null && status < 500,
+        ),
+      );
 
-      if (response.data != null) {
+      if (response.statusCode == 200 && response.data != null && response.data is Map<String, dynamic>) {
         var loginResponse = LoginResponseBody.fromJson(response.data);
         var headers = response.headers.map.map((key, value) => MapEntry(key, value.join(',')));
-        return (loginResponse, headers);
+        return ApiSuccess(loginResponse, headers: headers);
       } else {
-        throw Exception(response.data);
+        return ApiError(
+          statusCode: response.statusCode,
+          message: response.data.toString(),
+        );
       }
     } catch (e) {
-      throw Exception(e);
+      return const ApiError(message: "Network or unexpected error");
     }
   }
+
 }
