a2c50b5aec5400ae92c25fa83cde05f98be20bd6
Tanjid470 <tanjidamran11@gmail.com>
2025-04-27 17:51:47 +0600

    [ADDED] : Added `borderRadius` parameter to customize button border radius and updated BoxShadow offset.

--- lib/common_widget/button.dart
+++ lib/common_widget/button.dart
@@ -11,6 +11,7 @@ class Button2 extends StatelessWidget {
   final double? fontSize;
   final bool? scaleDown;
   final Widget? leading;
+  final double? borderRadius;
   final EdgeInsetsGeometry? padding;
   final EdgeInsetsGeometry? contentPadding;
   const Button2({
@@ -23,6 +24,7 @@ class Button2 extends StatelessWidget {
     this.scaleDown,
     this.leading,
     this.padding,
+    this.borderRadius,
     this.contentPadding,
   });
 
@@ -30,101 +32,101 @@ class Button2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return inActive != null && true
         ? Opacity(
-      opacity: 0.50,
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        decoration: ShapeDecoration(
-          color: const Color(0xFF6A5AE0),
-          shape: RoundedRectangleBorder(
-            side: const BorderSide(width: 1, color: Color(0xFF3A327B)),
-            borderRadius: BorderRadius.circular(29),
-          ),
-          shadows: const [
-            BoxShadow(
-              color: Color(0xFF3A327B),
-              blurRadius: 0,
-              offset: Offset(3, 3),
-              spreadRadius: 0,
-            )
-          ],
-        ),
-        child: Row(
-          mainAxisSize: scaleDown == true ? MainAxisSize.min : MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: fontSize ?? TextSize.font18(context),
-                  fontWeight: FontWeight.w500,
-                ),
+          opacity: 0.50,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            decoration: ShapeDecoration(
+              color: const Color(0xFF6A5AE0),
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(width: 1, color: Color(0xFF3A327B)),
+                borderRadius: BorderRadius.circular(29),
               ),
+              shadows: const [
+                BoxShadow(
+                  color: Color(0xFF3A327B),
+                  blurRadius: 0,
+                  offset: Offset(3, 3),
+                  spreadRadius: 0,
+                )
+              ],
             ),
-            icon ?? const SizedBox()
-          ],
-        ),
-      ),
-    )
+            child: Row(
+              mainAxisSize: scaleDown == true ? MainAxisSize.min : MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: fontSize ?? TextSize.font18(context),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                icon ?? const SizedBox()
+              ],
+            ),
+          ),
+        )
         : GestureDetector(
-      onTap: onClick,
-      child: Container(
-        margin: const EdgeInsets.only(right: 2),
-        padding: padding ?? EdgeInsets.symmetric(vertical: ResponsiveScale.of(context).hp(1)),
-        decoration: ShapeDecoration(
-          color: ColorUtils.competeBorderColor,
-          shape: RoundedRectangleBorder(
-            side: BorderSide(width: 1, color: ColorUtils.baseBlueColorShade700),
-            borderRadius: BorderRadius.circular(29),
-          ),
-          shadows: [
-            BoxShadow(
-              color: ColorUtils.baseBlueColorShade700,
-              blurRadius: 0,
-              offset: Offset(3, 3),
-              spreadRadius: 0,
-            )
-          ],
-        ),
-        child: Row(
-          mainAxisSize: scaleDown == true ? MainAxisSize.min : MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            leading ?? const SizedBox(),
-            scaleDown == true || leading != null
-                ? Padding(
-              // padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              padding: contentPadding ?? const EdgeInsets.only(left: 2, right: 12.0),
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: fontSize ?? TextSize.font18(context),
-                  fontWeight: FontWeight.w500,
-                ),
+          onTap: onClick,
+          child: Container(
+            margin: const EdgeInsets.only(right: 2),
+            padding: padding ?? EdgeInsets.symmetric(vertical: ResponsiveScale.of(context).hp(1)),
+            decoration: ShapeDecoration(
+              color: ColorUtils.competeBorderColor,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(width: 1, color: ColorUtils.baseBlueColorShade700),
+                borderRadius: BorderRadius.circular(borderRadius ?? 29),
               ),
-            )
-                : Expanded(
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: fontSize ?? TextSize.font18(context),
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+              shadows: [
+                BoxShadow(
+                  color: ColorUtils.baseBlueColorShade700,
+                  blurRadius: 0,
+                  offset: Offset(2, 2),
+                  spreadRadius: 0,
+                )
+              ],
             ),
-            icon ?? const SizedBox()
-          ],
-        ),
-      ),
-    );
+            child: Row(
+              mainAxisSize: scaleDown == true ? MainAxisSize.min : MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                leading ?? const SizedBox(),
+                scaleDown == true || leading != null
+                    ? Padding(
+                  // padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  padding: contentPadding ?? const EdgeInsets.only(left: 2, right: 12.0),
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: fontSize ?? TextSize.font18(context),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                )
+                    : Expanded(
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: fontSize ?? TextSize.font18(context),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                icon ?? const SizedBox()
+              ],
+            ),
+          ),
+        );
   }
 }
