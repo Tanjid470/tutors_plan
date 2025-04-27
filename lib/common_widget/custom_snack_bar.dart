01ed1d71dcbb482ca6b580276fa12a0afc7fc302
Tanjid470 <tanjidamran11@gmail.com>
2025-04-27 14:46:13 +0600

    [CREATE] : Implemented custom snack bar with title and subtitle.

--- lib/common_widget/custom_snack_bar.dart
+++ lib/common_widget/custom_snack_bar.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tutors_plan/config/font_constants.dart';
+import 'package:tutors_plan/const/text_style.dart';
 
-SnackBar customSnackBar(String title,{Color? color, String? message}) {
+SnackBar customSnackBar(String title, BuildContext context,{Color? color, String? subtitle}) {
   return SnackBar(
-    content: Text(title),
+    content: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title,style: whiteText(TextSize.font18(context)),),
+        Text(subtitle ?? '',style: whiteText(TextSize.font14(context),fontWeight: FontWeight.w400),)
+      ],
+    ),
     duration: const Duration(seconds: 2),
     action: SnackBarAction(
       label: 'Close',
+      textColor: Colors.white,
       onPressed: () {
         Get.back();
       },),
