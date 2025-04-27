b3b4cfd32c2cc7d41e3b5728452e3f0d9d55d19d
Tanjid470 <tanjidamran11@gmail.com>
2025-04-27 14:46:24 +0600

    [UPDATE] : Updated success and error snack bar colors to use shade 200 of green and red respectively

--- lib/const/color_utils.dart
+++ lib/const/color_utils.dart
@@ -19,6 +19,6 @@ class ColorUtils{
   static Color errorBorderColor = Colors.red;
   static Color competeBorderColor = baseBlueColor;
 
-  static Color successSnackBarColor = Colors.green.shade300;
-  static Color errorSnackBarColor = Colors.red.shade300;
+  static Color successSnackBarColor = Colors.green.shade200;
+  static Color errorSnackBarColor = Colors.red.shade200;
 }
