cc2d1737c59f983c6c9487a9600dbc2410ab2ce5
Tanjid470 <tanjidamran11@gmail.com>
2025-04-27 16:16:04 +0600

    [CREATE] : Added user role card widget with title and image. Use card with select style.

---
+++ lib/feature/register/view/widget/user_role_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tutors_plan/config/font_constants.dart';
+import 'package:tutors_plan/const/color_utils.dart';
+import 'package:tutors_plan/const/text_style.dart';
+class UserRoleCard extends StatelessWidget {
+  const UserRoleCard({
+    super.key,
+    required this.context,
+    required this.title,
+    this.imageUrl,
+    this.color,
+    this.isSelected = false,
+  });
+
+  final BuildContext context;
+  final String title;
+  final String? imageUrl;
+  final Color? color;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color: isSelected ? color ?? ColorUtils.baseBlueColor : Colors.white,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            imageUrl ?? 'assets/svg/graduation_cap.svg',
+            width: 30,
+            height: 30,
+          ),
+          Text(
+            title,
+            style: customTextStyle(
+              context,
+              fontSize: TextSize.font18(context),
+              color: ColorUtils.black,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
