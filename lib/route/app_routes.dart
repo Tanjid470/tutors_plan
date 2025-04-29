23b94752bf187efd9c282257c1c05063a4b9662f
Tanjid470 <tanjidamran11@gmail.com>
2025-04-29 18:46:50 +0600

    [ADDED] : Added route for bottom navigation widget

--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -1,6 +1,7 @@
 part of 'app_pages.dart';
 abstract class RouteNames {
   RouteNames._();
+  static const bottomNavigationWidget = _Paths.bottomNavigationWidget;
   static const onboardView = _Paths.onboardView;
   static const dashboardView = _Paths.dashboardView;
   static const loginView = _Paths.loginView;
@@ -8,6 +9,7 @@ abstract class RouteNames {
 
 abstract class _Paths {
   _Paths._();
+  static const bottomNavigationWidget = '/bottom_navigation_widget';
   static const onboardView = '/onboard_view';
   static const dashboardView = '/dashboard_view';
   static const loginView = '/login_view';
