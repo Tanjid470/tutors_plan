ad8e19a4842aefaa018001439e4d6b9869e275ef
Tanjid470 <tanjidamran11@gmail.com>
2025-04-29 19:27:55 +0600

    [ADDED] : Added black text style with custom font size and weight

--- lib/const/text_style.dart
+++ lib/const/text_style.dart
@@ -9,6 +9,13 @@ TextStyle whiteText(double fontSize,{fontWeight}){
       color:Colors.white
   );
 }
+TextStyle blackText(double fontSize,{fontWeight}){
+  return  TextStyle(
+      fontSize: fontSize,
+      fontWeight: fontWeight ?? FontWeight.w700,
+      color:Colors.black
+  );
+}
 TextStyle customTextStyle(context,{Color? color,double? fontSize,fontWeight}){
   return  TextStyle(
       fontSize: fontSize ?? TextSize.font14(context),
