a4045d28e488923bcaad6c46295a80caed86948e
Tanjid470 <tanjidamran11@gmail.com>
2025-04-30 16:36:52 +0600

    [ADDED] : Added image assets for dashboard, learning art, scholar pass, and tutor learning posters.

--- lib/const/app_images.dart
+++ lib/const/app_images.dart
@@ -1,3 +1,6 @@
 class AppImages{
   static final dashBoard = 'assets/images/tutorsPlan_logo.png';
+  static final learningARTPoster = 'assets/images/learning_art_poster.png';
+  static final scholarPassPoster = 'assets/images/scholar_pass_poster.png';
+  static final tutorLearningPoster = 'assets/images/learning_tutor_poster.png';
 }
