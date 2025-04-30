aab31b714d0ed20952274b14a0fea2ed5285835b
Tanjid470 <tanjidamran11@gmail.com>
2025-04-30 17:00:28 +0600

    [ADDED] : Added icons for home navigation

--- lib/const/app_images.dart
+++ lib/const/app_images.dart
@@ -3,4 +3,11 @@ class AppImages{
   static final learningARTPoster = 'assets/images/learning_art_poster.png';
   static final scholarPassPoster = 'assets/images/scholar_pass_poster.png';
   static final tutorLearningPoster = 'assets/images/learning_tutor_poster.png';
+
+
+
+  //icons
+  static final homeSelected = 'assets/icons/home_selected.png';
+  static final homeUnSelected = 'assets/icons/home_unselected.png';
+
 }
