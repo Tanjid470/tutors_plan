b9eb2af6a9b3165d76bc30127b022feaf4198fa7
Tanjid470 <tanjidamran11@gmail.com>
2025-05-03 13:05:32 +0600

    [REMOVE] : Import only needed classes in dashboard controller

--- lib/feature/dashboard/controller/dashboard_controller.dart
+++ lib/feature/dashboard/controller/dashboard_controller.dart
@@ -1,16 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tutors_plan/common_widget/custom_snack_bar.dart';
-import 'package:tutors_plan/const/color_utils.dart';
-import 'package:tutors_plan/const/enums.dart';
 import 'package:tutors_plan/feature/dashboard/data/course_categories_response_body.dart';
 import 'package:tutors_plan/feature/dashboard/data/repository/dashboard_repository.dart';
-import 'package:tutors_plan/feature/login/data/login_response_body.dart';
-import 'package:tutors_plan/feature/login/data/repository/login_repository.dart';
-import 'package:tutors_plan/feature/login/domain/login_body.dart';
-import 'package:tutors_plan/main.dart';
-import 'package:tutors_plan/route/app_pages.dart';
-import 'package:tutors_plan/utils/network/api_result.dart';
+
 
 class DashboardController extends GetxController{
 
