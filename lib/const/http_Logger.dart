d5bb18f5e77d201ab641c62ecfd8a5f80a4d0820
Tanjid470 <tanjidamran11@gmail.com>
2025-05-03 13:17:31 +0600

    DELETE

--- lib/const/http_Logger.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:pretty_http_logger/pretty_http_logger.dart';
-
-HttpWithMiddleware httpLog = HttpWithMiddleware.build(
-    middlewares: [
-      HttpLogger(
-        logLevel: LogLevel.BODY,
-      ),
-    ]);
