9c7ffd50db17315bb612f06620f3ff0958512512
Tanjid470 <tanjidamran11@gmail.com>
2025-05-03 12:44:36 +0600

    [REMOVE]: Removed state management variables and added loading state for category list.

--- lib/feature/dashboard/controller/dashboard_controller.dart
+++ lib/feature/dashboard/controller/dashboard_controller.dart
@@ -13,31 +13,22 @@ import 'package:tutors_plan/route/app_pages.dart';
 import 'package:tutors_plan/utils/network/api_result.dart';
 
 class DashboardController extends GetxController{
-  final Rx<ScreenStates> screenStates = Rx<ScreenStates>(ScreenStates.DEFAULT);
-  final Rx<ScreenStates> loaderState = Rx<ScreenStates>(ScreenStates.DEFAULT);
 
   DashboardRepository dashboardRepository = DashboardRepository();
 
   List<CategoryListModel>? categoryList = [];
 
+  RxBool isLoadingCategoryList = false.obs;
+
   Future<void> getCourseCategory() async {
-    updateViewState(loadingState: ScreenStates.TRANSPARENT_LOADING_START);
     final result = await dashboardRepository.getCourseCategory(
       page: 1.toString(),
       limit:10.toString()
     );
     if (result != null) {
+      isLoadingCategoryList.value = true;
       categoryList = result;
     }
-    updateViewState(screenStates: ScreenStates.LOADING_COMPLETE);
   }
 
-  void updateViewState({ScreenStates? screenStates, ScreenStates? loadingState}) {
-    if (screenStates != null) {
-      this.screenStates.value = screenStates;
-      loaderState.value = ScreenStates.LOADING_COMPLETE;
-    }
-    if (loadingState != null) loaderState.value = loadingState;
-    update();
-  }
 }
