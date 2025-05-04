a9bc952685dafc4d936a36ec41a90e922985c2ef
Tanjid470 <tanjidamran11@gmail.com>
2025-05-04 13:53:44 +0600

    [UPDATE] : Redesign dashboard view with new layout and features.

--- lib/feature/dashboard/view/dashboard_view.dart
+++ lib/feature/dashboard/view/dashboard_view.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
-import 'package:get/get.dart';
-import 'package:tutors_plan/common_widget/button.dart';
-import 'package:tutors_plan/common_widget/loading_view_transparent.dart';
-import 'package:tutors_plan/config/font_constants.dart';
+import 'package:get/get.dart';import 'package:tutors_plan/config/font_constants.dart';
 import 'package:tutors_plan/config/responsive_scale.dart';
 import 'package:tutors_plan/const/color_utils.dart';
-import 'package:tutors_plan/const/enums.dart';
 import 'package:tutors_plan/const/text_style.dart';
 import 'package:tutors_plan/feature/dashboard/controller/dashboard_controller.dart';
 import 'package:tutors_plan/feature/dashboard/widget/course_card.dart';
+import 'package:tutors_plan/feature/dashboard/widget/scholar_pass_view.dart';
 import 'package:tutors_plan/main.dart';
 import 'package:tutors_plan/route/app_pages.dart';
 
@@ -34,7 +31,7 @@ class _DashboardViewState extends State<DashboardView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: Column(
           children: [
             verticalGap(context, 5),
@@ -43,119 +40,11 @@ class _DashboardViewState extends State<DashboardView> {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
+                  spacing: ResponsiveScale.of(context).hp(1),
                   children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.435,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 10,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return CourseCard();
-                        },
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Text('Categories',
-                          style: customTextStyle(
-                              context,
-                              fontSize: TextSize.font20(context),
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const Spacer(),
-              
-                        Text('See all',
-                            style: customTextStyle(
-                              context,
-                              fontSize: TextSize.font14(context),
-                            )
-                        ),
-                      ],
-                    ),
-                    Obx((){
-                      return dashboardController.isLoadingCategoryList.value
-                          ? SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.15,
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              padding: EdgeInsets.zero,
-                              itemCount: dashboardController.categoryList?.length,
-                              itemBuilder: (context, index) {
-                                final categoryItem = dashboardController.categoryList?[index];
-                                return Container(
-                                  height: MediaQuery.of(context).size.height * 0.15,
-                                  width: MediaQuery.of(context).size.width * 0.75,
-                                  padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-                                  margin: const EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                      color: Colors.transparent,
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.grey.shade200,
-                                          blurStyle: BlurStyle.outer,
-                                          offset: Offset(0, 0),
-                                          blurRadius: 5,
-                                        ),
-                                      ],
-                                      borderRadius: BorderRadius.all(Radius.circular(10))
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(12),
-                                    child: Row(
-                                      children: [
-                                        Image.network(
-                                          categoryItem?.image ?? '',
-                                          width: 70,
-                                          height: 70,
-                                          fit: BoxFit.cover,
-                                        ),
-                                        SizedBox(width: ResponsiveScale.of(context).wp(2)),
-                                        Expanded(
-                                          child: Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text(categoryItem?.name ?? '',
-                                                  maxLines: 1,
-                                                  overflow: TextOverflow.ellipsis,
-                                                  style: TextStyle(
-                                                      fontWeight: FontWeight.bold, fontSize: TextSize.font16(context))
-                                              ),
-                                              Text(
-                                                "${categoryItem?.seoKeywords.toString() ?? ''} courses",
-                                                maxLines: 2,
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                              const Spacer(),
-                                              Row(
-                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                children: [
-                                                  Text(
-                                                    "${categoryItem?.totalCourse.toString() ?? ''} courses",
-                                                    maxLines: 2,
-                                                    overflow: TextOverflow.ellipsis,
-                                                  ),
-                                                  Text(
-                                                    "${categoryItem?.enrolledStudents.toString() ?? ''} students",
-                                                    maxLines: 2,
-                                                    overflow: TextOverflow.ellipsis,
-                                                  ),
-                                                ],
-                                              ),
-
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          )
-                          : SizedBox();
-                    })
+                    courseCardView(),
+                    programCardView(),
+                    categoryView(),
                   ],
                 ),
               ),
@@ -224,6 +113,202 @@ class _DashboardViewState extends State<DashboardView> {
           ],
         )
       ],
+    );
+  }
+
+  Widget categoryView(){
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text('Categories',
+              style: customTextStyle(
+                  context,
+                  fontSize: TextSize.font20(context),
+                  fontWeight: FontWeight.bold),
+            ),
+            const Spacer(),
+
+            Text('See all',
+                style: customTextStyle(
+                  context,
+                  fontSize: TextSize.font14(context),
+                )
+            ),
+          ],
+        ),
+        Obx((){
+          return dashboardController.isLoadingCategoryList.value
+              ? SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.zero,
+              itemCount: dashboardController.categoryList?.length,
+              itemBuilder: (context, index) {
+                final categoryItem = dashboardController.categoryList?[index];
+                return Container(
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                  margin: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade200,
+                          blurStyle: BlurStyle.outer,
+                          offset: Offset(0, 0),
+                          blurRadius: 5,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Row(
+                      children: [
+                        Image.network(
+                          categoryItem?.image ?? '',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(width: ResponsiveScale.of(context).wp(2)),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(categoryItem?.name ?? '',
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold, fontSize: TextSize.font16(context))
+                              ),
+                              Text(
+                                "${categoryItem?.seoKeywords.toString() ?? ''} courses",
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const Spacer(),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "${categoryItem?.totalCourse.toString() ?? ''} courses",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(
+                                    "${categoryItem?.enrolledStudents.toString() ?? ''} students",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),
+
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+              : SizedBox();
+        })
+      ]
+    );
+  }
+
+  Widget courseCardView(){
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.43,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        padding: EdgeInsets.symmetric(vertical: 10),
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return CourseCard();
+        },
+      ),
+    );
+  }
+
+  Widget programCardView(){
+    return Row(
+      spacing: 10,
+      children: [
+        Expanded(
+          child: ProgramCard(
+            title: 'ScholarPASS',
+            icon: Icons.school_outlined,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ScholarPassScreen()),
+              );
+            },
+
+          ),
+        ),
+
+        Expanded(
+          child: ProgramCard(
+            title: 'LearningART',
+            onTap: () {
+            },
+            icon: Icons.style_outlined,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ProgramCard extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  final IconData icon;
+  const ProgramCard({
+    super.key, required this.title, required this.onTap, required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [ColorUtils.baseBlueColorShade700, ColorUtils.baseBlueColorShade300],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          spacing: ResponsiveScale.of(context).hp(1),
+          children: [
+            Icon(icon,color: ColorUtils.white,size: TextSize.font36(context),),
+            Text(title,
+              style: customTextStyle(
+                context,
+                fontSize: TextSize.font24(context),
+                fontWeight: FontWeight.bold,
+                color: ColorUtils.white
+              )
+            ),
+
+          ],
+        )
+      ),
     );
   }
 }
