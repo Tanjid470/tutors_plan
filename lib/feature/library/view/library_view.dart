ca8a7509b9107d984dc9b564e4d0822083ab9bae
Tanjid470 <tanjidamran11@gmail.com>
2025-05-07 15:49:02 +0600

    [ADDED] : Added Live Tutoring with Zego Cloud integration for video conferencing

--- lib/feature/library/view/library_view.dart
+++ lib/feature/library/view/library_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:tutors_plan/common_widget/text_tab_bar.dart';
 import 'package:tutors_plan/config/font_constants.dart';
 import 'package:tutors_plan/const/color_utils.dart';
+import 'package:zego_uikit_prebuilt_video_conference/zego_uikit_prebuilt_video_conference.dart';
 
 class LibraryView extends StatefulWidget {
   const LibraryView({super.key});
@@ -12,6 +14,7 @@ class LibraryView extends StatefulWidget {
 
 class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStateMixin{
   TabController? _tabController;
+  TextEditingController meetsTabController = TextEditingController();
   final _tabs = const ['Self Learn', 'Live Tutoring', 'Stats'];
   @override
   void initState() {
@@ -115,11 +118,70 @@ class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStat
   }
 
   Widget _game() {
-    return Center(child: Text('Live Tutoring',style: TextStyle(fontSize: TextSize.font20(context),fontWeight: FontWeight.bold)));
+    return Expanded(
+      child: Column(
+        children: [
+          Text('Live Tutoring',style: TextStyle(fontSize: TextSize.font20(context),fontWeight: FontWeight.bold)),
+          Image.network('https://w7.pngwing.com/pngs/805/460/png-transparent-zoom-logo-thumbnail.png',
+            width: MediaQuery.of(context).size.width * 0.8,
+          ),
+          Text('User Id: 1234567890'),
+          Text('Password: 1234567890'),
+          TextFormField(
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            controller: meetsTabController,
+            decoration: const InputDecoration(
+              hintText: 'Enter User Id',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              jumpToMeetPage(context, conferenceId : meetsTabController.text);
+            },
+            child: Text('Join a meeting'),
+          ),
+          SizedBox(height: 10,),
+          ElevatedButton(
+            onPressed: () {
+              jumpToMeetPage(context, conferenceId : '12345678');
+            },
+            child: Text('New meeting'),
+          )
+
+        ],
+      )
+    );
   }
 
   Widget _exam() {
     return Center(child: Text('Stats',style: TextStyle(fontSize: TextSize.font20(context),fontWeight: FontWeight.bold)));
   }
 
+  void jumpToMeetPage(BuildContext context, {required String conferenceId}) {
+    Navigator.push(context,
+      MaterialPageRoute(builder: (context)=> VideoConferencePage(conferenceId :conferenceId))
+    );
+  }
+}
+
+class VideoConferencePage extends StatelessWidget {
+  final String conferenceId;
+  VideoConferencePage({super.key, required this.conferenceId});
+  final int appID = int.parse(dotenv.get('ZEGO_APP_ID'));
+  final String appSIGN = dotenv.get('ZEGO_APP_SIGN');
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ZegoUIKitPrebuiltVideoConference(
+        appID: appID,
+        appSign: appSIGN,
+        conferenceID: conferenceId,
+        config: ZegoUIKitPrebuiltVideoConferenceConfig(),
+        userID: '1234',
+        userName: 'Jawad',
+      )
+    );
+  }
 }
