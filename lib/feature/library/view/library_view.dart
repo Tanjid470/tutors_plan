ae9e680957745c103b6cdacdc593f8015afa583e
Tanjid470 <tanjidamran11@gmail.com>
2025-05-08 15:51:07 +0600

    [ADDED] : Added self learn, live tutoring, and stats views with meeting feature

--- lib/feature/library/view/library_view.dart
+++ lib/feature/library/view/library_view.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tutors_plan/common_widget/buttons.dart';
 import 'package:tutors_plan/common_widget/text_tab_bar.dart';
 import 'package:tutors_plan/config/font_constants.dart';
 import 'package:tutors_plan/const/color_utils.dart';
+import 'package:tutors_plan/const/enums.dart';
+import 'package:tutors_plan/const/text_style.dart';
 import 'package:zego_uikit_prebuilt_video_conference/zego_uikit_prebuilt_video_conference.dart';
 
 class LibraryView extends StatefulWidget {
@@ -16,6 +20,13 @@ class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStat
   TabController? _tabController;
   TextEditingController meetsTabController = TextEditingController();
   final _tabs = const ['Self Learn', 'Live Tutoring', 'Stats'];
+
+  final String userId = Random().nextInt(900000 + 100000).toString();
+  final String randomConferenceId =
+    (Random().nextInt(1000000000) * 10 +
+        Random().nextInt(10)).toString()
+        .padLeft(10, '0');
+
   @override
   void initState() {
     super.initState();
@@ -27,7 +38,7 @@ class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStat
     return   Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
@@ -41,7 +52,6 @@ class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStat
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              const SizedBox(height: 20),
               Expanded(child: body()),
             ],
           ),
@@ -76,15 +86,14 @@ class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStat
         parent: NeverScrollableScrollPhysics(),
       ),
       children: [
-        _quiz(),
-        _game(),
-        _exam(),
-
+        _selfLearning(),
+        _liveTutoring(),
+        _stats(),
       ],
     );
   }
 
-  Widget _quiz() {
+  Widget _selfLearning() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -117,44 +126,91 @@ class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStat
     );
   }
 
-  Widget _game() {
+  Widget _liveTutoring() {
     return Expanded(
-      child: Column(
-        children: [
-          Text('Live Tutoring',style: TextStyle(fontSize: TextSize.font20(context),fontWeight: FontWeight.bold)),
-          Image.network('https://w7.pngwing.com/pngs/805/460/png-transparent-zoom-logo-thumbnail.png',
-            width: MediaQuery.of(context).size.width * 0.8,
-          ),
-          Text('User Id: 1234567890'),
-          Text('Password: 1234567890'),
-          TextFormField(
-            maxLength: 10,
-            keyboardType: TextInputType.number,
-            controller: meetsTabController,
-            decoration: const InputDecoration(
-              hintText: 'Enter User Id',
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: [
+            Image.asset('assets/images/zoom_logo.png',
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.height * 0.25,
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              jumpToMeetPage(context, conferenceId : meetsTabController.text);
-            },
-            child: Text('Join a meeting'),
-          ),
-          SizedBox(height: 10,),
-          ElevatedButton(
-            onPressed: () {
-              jumpToMeetPage(context, conferenceId : '12345678');
-            },
-            child: Text('New meeting'),
-          )
+            Text('Your userId : $userId',
+              style: customTextStyle(
+                context,
+                fontSize: TextSize.font16(context),
+                fontWeight: FontWeight.w500,
+                color: Colors.black87
+              ),
+            ),
+            verticalGap(context, 1),
+            Text('Conference Id : $randomConferenceId',
+              style: customTextStyle(
+                  context,
+                  fontSize: TextSize.font16(context),
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black87
+              ),
+            ),
+            verticalGap(context, 2),
+            TextFormField(
+              maxLength: 10,
+              keyboardType: TextInputType.number,
+              controller: meetsTabController,
+              decoration: const InputDecoration(
+                hintText: 'Enter conference Id to join',
+                hintStyle: TextStyle(color: Colors.grey),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+                ),
+              ),
+            ),
+            Buttons(
+              onTap: () {
+                if (meetsTabController.text.length == 10) {
+                  jumpToMeetPage(context, conferenceId: meetsTabController.text);
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Conference ID must be 10 digits',
+                        style: customTextStyle(
+                          context,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: TextSize.font16(context)
+                        )
+                      ),
+                      behavior: SnackBarBehavior.floating,
+                      duration: Duration(seconds: 2),
+                      backgroundColor: ColorUtils.errorSnackBarColor,
+                    ),
+                  );
+                }
+              },
+              title: 'Join a meeting',
+              style: ButtonsStyle.blueButton,
+            ),
+            SizedBox(height: 10,),
+            Buttons(
+              onTap: () {
+                jumpToMeetPage(context, conferenceId : randomConferenceId);
+              },
+              title: 'Start a meeting',
+              style: ButtonsStyle.blueButton,
+            )
 
-        ],
+          ],
+        ),
       )
     );
   }
 
-  Widget _exam() {
+  Widget _stats() {
     return Center(child: Text('Stats',style: TextStyle(fontSize: TextSize.font20(context),fontWeight: FontWeight.bold)));
   }
 
@@ -167,6 +223,8 @@ class _LibraryViewState extends State<LibraryView> with SingleTickerProviderStat
 
 class VideoConferencePage extends StatelessWidget {
   final String conferenceId;
+  final String userId = Random().nextInt(900000 + 100000).toString();
+
   VideoConferencePage({super.key, required this.conferenceId});
   final int appID = int.parse(dotenv.get('ZEGO_APP_ID'));
   final String appSIGN = dotenv.get('ZEGO_APP_SIGN');
@@ -179,8 +237,8 @@ class VideoConferencePage extends StatelessWidget {
         appSign: appSIGN,
         conferenceID: conferenceId,
         config: ZegoUIKitPrebuiltVideoConferenceConfig(),
-        userID: '1234',
-        userName: 'Jawad',
+        userID: userId,
+        userName: 'Tanjid',
       )
     );
   }
