92dbed636f9660ac0ff394a80d3727a60a505f98
Tanjid470 <tanjidamran11@gmail.com>
2025-05-10 14:10:53 +0600

    [REMOVE]:removed SmartDialog

--- lib/feature/dashboard/view/dashboard_view.dart
+++ lib/feature/dashboard/view/dashboard_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:tutors_plan/config/font_constants.dart';
 import 'package:tutors_plan/config/responsive_scale.dart';
