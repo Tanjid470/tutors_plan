644bda7d9a33367a50a445caca80f58e780fe8f3
Tanjid470 <tanjidamran11@gmail.com>
2025-05-21 15:23:16 +0600

    [UPDATE]: course details data fetch

--- lib/feature/course_details/controller/course_details_controller.dart
+++ lib/feature/course_details/controller/course_details_controller.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import 'package:tutors_plan/const/enums.dart';
 import 'package:tutors_plan/feature/course_details/data/course_details_response_body.dart';
 import 'package:tutors_plan/feature/course_details/data/repository/course_details_repository.dart';
-import 'package:tutors_plan/feature/dashboard/data/course_categories_response_body.dart';
 
 
 class CourseDetailsController extends GetxController {
@@ -10,13 +9,15 @@ class CourseDetailsController extends GetxController {
   final Rx<ScreenStates> loaderState = Rx<ScreenStates>(ScreenStates.DEFAULT);
 
   CourseDetailsRepository courseDetailsRepository = CourseDetailsRepository();
-  CourseDetails courseDetails = CourseDetails();
+  final Rx<CourseDetails> courseDetails = CourseDetails().obs;
+
 
   Future<void> getCourseDetails() async {
     updateViewState(loadingState: ScreenStates.TRANSPARENT_LOADING_START);
     final result = await courseDetailsRepository.getCourseDetailsResponse();
     if (result != null) {
-      courseDetails = result;
+      courseDetails.value = result;
+      updateViewState(screenStates: ScreenStates.LOADING_COMPLETE);
     }
     updateViewState(screenStates: ScreenStates.LOADING_COMPLETE);
   }
