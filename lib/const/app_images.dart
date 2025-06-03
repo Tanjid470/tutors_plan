45f7e9e89dd36ab3a32e8a275b03e645206be7c9
Tanjid470 <tanjidamran11@gmail.com>
2025-06-03 14:39:58 +0600

    [ADDED]: App flutter_launcher_icons icon

--- lib/const/app_images.dart
+++ lib/const/app_images.dart
@@ -1,5 +1,5 @@
 class AppImages{
-  static final dashBoard = 'assets/images/tutorsPlan_logo.png';
+  static final dashBoard = 'assets/images/tutorsPlan.png';
   static final learningARTPoster = 'assets/images/learning_art_poster.png';
   static final scholarPassPoster = 'assets/images/scholar_pass_poster.png';
   static final tutorLearningPoster = 'assets/images/learning_tutor_poster.png';
