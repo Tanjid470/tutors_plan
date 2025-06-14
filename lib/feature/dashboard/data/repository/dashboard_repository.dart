edb95978945f66399a68d436720f373c9bed48e5
Tanjid470 <tanjidamran11@gmail.com>
2025-06-14 19:38:38 +0600

    [ADDED]: Add `getCategory` to `DashboardRepository`

--- lib/feature/dashboard/data/repository/dashboard_repository.dart
+++ lib/feature/dashboard/data/repository/dashboard_repository.dart
@@ -6,6 +6,8 @@ import 'package:tutors_plan/feature/profile/data/profile_get_response.dart';
 import 'package:tutors_plan/main.dart';
 import 'package:tutors_plan/utils/network/api_client.dart';
 
+import '../../../category/data/category_response.dart';
+
 class DashboardRepository {
  Dio dio = Dio();
 
@@ -32,6 +34,38 @@ class DashboardRepository {
      CoursesResponse data = CoursesResponse.fromJson(response.data);
      if (data.status == 200) {
        return data.results?.data;
+     }
+     else {
+       return null;
+     }
+   } catch (e) {
+     debugPrint(e.toString());
+   }
+   return null;
+ }
+
+ Future<List<Category>?> getCategory({int? page, int? limit,}) async {
+   try {
+     final int currentPage = page ?? 1;
+     final queryParameters = {
+       "pagination": "true",
+       "page": "$currentPage",
+       "limit": "${(limit ?? 10) * currentPage}",
+     };
+
+     dio = await ApiClient.dioClient(true);
+     var accessToken = preferences.getString('accessToken');
+     Response response = await dio.get(
+       UrlConst.getCourseCategoryEndpoint,
+       options: Options(
+         headers: {
+           'Authorization': 'Bearer $accessToken',
+         },
+       ),
+     );
+     CategoryResponse data = CategoryResponse.fromJson(response.data);
+     if (data.status == 200) {
+       return data.categories;
      }
      else {
        return null;
